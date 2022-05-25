aeaf04bf0dc28317c7577e57487cda508f19264a
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-05-25 14:36:29 +0200

    Changed language

--- wizytowki.py
+++ wizytowki.py
@@ -2,146 +2,144 @@ from faker import Faker
 fake = Faker()
 
 class Card:
-    def __init__(self,typ,imie, telefon):
+    def __init__(self,typ, name, phone):
         self.typ = typ
-        self.imie = imie
-        self.telefon = telefon
+        self.name = name
+        self.phone = phone
         
         
 class BaseContact(Card):
-    def __init__(self,rodzaj,imie, telefon, mail):
-       super().__init__("Base",imie,telefon)
-       self.rodzaj = rodzaj
+    def __init__(self, type, name, phone, mail):
+       super().__init__("Base", name, phone)
+       self.type = type
        self.mail = mail
     
     def contact(self):
-        print(f"Wybieram numer {self.telefon} i dzwonie do {self.imie}")
+        print(f"I dial the number {self.phone} and call {self.name}")
     
     def label_length(self):
-        długość = f"Długość imienia i nazwiska: {len(self.imie)}"
-        #print(f"Długość imienia i nazwiska wynosi: {długość}")
-        return długość
+        length = f"The length of the first and last name: {len(self.name)}"
+        return length
     
     def info(self):
-        information = "Typ wizytówki: " + self.rodzaj + "\n" + "Imię:"+ " " + self.imie + "\n" + "Nr telefonu:" + " "+ self.telefon + "\n" + "Mail:" + " "+ self.mail
+        information = "Card type: " + self.type + "\n" + "Name:"+ " " + self.name + "\n" + "Phone number:" + " "+ self.phone + "\n" + "Mail:" + " "+ self.mail
         return information
 
 
 class BusinessContact(Card):
-    def __init__(self,rodzaj, imie,stanowisko, firma, telefonsł):
-        super().__init__("Business",imie, telefonsł)
-        self.pozycja = stanowisko
-        self.firma = firma
-        self.telefonsł = telefonsł
-        self.rodzaj = rodzaj
+    def __init__(self, type, name, state, company, phone):
+        super().__init__("Business",name, phone)
+        self.state = state
+        self.company = company
+        self.phone = phone
+        self.type = type
         
     def label_length(self):
-        długość = f"Długość imienia i nazwiska: {len(self.imie)}"
-        #print(f"Długość imienia i nazwiska wynosi: {długość}")
-        return długość    
+        length = f"The length of the first and last name: {len(self.name)}"
+        return length    
        
     
     def contact(self):
-        print(f"Wybieram numer {self.telefonsł} i dzwonie do {self.imie}")
+        print(f"I dial the number {self.phone} and call {self.name}")
 
     def info(self):
-        information = "Typ wizytówki: " + self.rodzaj + "\n" + "Imię: " + self.imie + "\n" + "Stanowisko: " + self.firma + "\n" + "Firma: " + self.pozycja + "\n" + "Telefon służbowy: " + self.telefonsł
+        information = "Card type: " + self.type + "\n" + "Name: " + self.name + "\n" + "State: " + self.company + "\n" + "Company: " + self.state + "\n" + "Phone number: " + self.phone
         return information
 
 
-class Kontaktbusiness():
-    def __init__(self, rodzaj, imię, telefonsł, firma, stanowisko):
-        super().__init__("Business",telefonsł,imię)
-        self.imię = imię
-        self.telsł = telefonsł
-        self.firma = firma
-        self.stanowisko = stanowisko
-        self.rodzaj = rodzaj
+class Contactbusiness():
+    def __init__(self, type, name, phone, company, state):
+        super().__init__("Business",phone,name)
+        self.name = name
+        self.phone = phone
+        self.company = company
+        self.state = state
+        self.type = type
     
     def contact(self):
-        contact = f"Wybieram numer {self.telsł} i dzwonie do {self.imię}"
+        contact = f"I dial the number {self.phone} and call {self.name}"
         return contact
 
-class Kontaktbase():
-    def __init__(self, imie, telefon):
-        super().__init__("Base",telefon, imie)
-        self.imie = imie
-        self.telefon = telefon
+class Contactbase():
+    def __init__(self, name, phone):
+        super().__init__("Base",phone, name)
+        self.name = name
+        self.phone = phone
         
 
     def contact(self):
-        contact = f"Wybieram numer {self.telefon} i dzwonie do {self.imie}"
+        contact = f"I dial the number {self.phone} and call {self.name}"
         return contact
 
 
 class Organizer:
-    właściciel = ""
-    bazaWizytowek = []
-    def __init__(self, właściciel):
-        self.właściciel = właściciel
+    owner = ""
+    cardsBase = []
+    def __init__(self, owner):
+        self.owner = owner
     
-    def dodajWizytówkę(self):
-        rodzaj = input("Rodzaj wizytówki (Base/Business): ")
-        if rodzaj == 'Base':
-            imię = fake.name()
+    def addCard(self):
+        type = input("Pick a card type (Base/Business): ")
+        if type == 'Base':
+            name = fake.name()
             mail = fake.email()
-            telefon = fake.country_calling_code() + fake.msisdn()[4:]
-            nowaWizytowka = BaseContact(rodzaj,imię, mail, telefon)
-            self.bazaWizytowek.append(nowaWizytowka)
-            print("Pomyślnie dodano wizytówkę")
+            phone = fake.country_calling_code() + fake.msisdn()[4:]
+            newCard = BaseContact(type, name, mail, phone)
+            self.cardsBase.append(newCard)
+            print("Successfully added new card")
             
-        elif rodzaj == 'Business':
-            imię = fake.name()
-            firma = fake.company()
-            stanowisko = fake.job()
-            telefonsł = fake.country_calling_code() + fake.msisdn()[4:]
-            nowaWizytowka = BusinessContact(rodzaj,imię, firma, stanowisko, telefonsł)
-            self.bazaWizytowek.append(nowaWizytowka)
-            print("Pomyślnie dodano wizytówkę")
-    def wyświetlWizytówki(self):
-        print('Lista wizytówek: ')
-        for i in self.bazaWizytowek:
-            if i.typ == 'Base':
+        elif type == 'Business':
+            name = fake.name()
+            company = fake.company()
+            state = fake.job()
+            phone = fake.country_calling_code() + fake.msisdn()[4:]
+            newCard = BusinessContact(type, name, company, state, phone)
+            self.cardsBase.append(newCard)
+            print("Successfully added new card")
+    def showCards(self):
+        print('Cards list: ')
+        for i in self.cardsBase:
+            if i.type == 'Base':
                 print(i.info())
                 print(i.label_length())
                 print("\n")
             
-            elif i.typ == 'Business':
+            elif i.type == 'Business':
                 print(i.info())
                 print(i.label_length())
                 print("\n")
             
                        
-    def kontaktbase(self):
-        print("Kontakt z osobami z listy BaseContact: ")
-        for i in self.bazaWizytowek:
-            if i.typ == 'Base':
-                print(f"Wybieram numer {i.mail} i dzwonie do {i.imie}")
+    def contactbase(self):
+        print("Contact people on the BaseContact list: ")
+        for i in self.cardsBase:
+            if i.type == 'Base':
+                print(f"I dial the number {i.mail} and call {i.name}")
                           
             
-    def kontaktbusiness(self):      
-        print("Kontakt z osobami z listy BusinessContact: ")
-        for i in self.bazaWizytowek:
-            if i.typ == 'Business':
-                print(f"Wybieram numer {i.telefonsł} i dzwonie do {i.imie}")
+    def contactbusiness(self):      
+        print("Contact people on the BusinessContact list: ")
+        for i in self.cardsBase:
+            if i.type == 'Business':
+                print(f"I dial the number {i.phone} and call {i.name}")
         
 def main():                
-    organizerAnalityka = Organizer("Adam")
+    organizer = Organizer("Owner")
 
     while True:
-        print("""Co chcesz zrobić?:    
-                1- Dodać wizytówkę
-                2- Wyświetlić wizytówki
-                3- Kontakt z osobami z wizytówki BaseContact
-                4- Kontakt z osobami z wizytówki BusinessContact
-                5- Wyjście z programu""")
-        x = input("Wybrany numer: ")
+        print("""What would you like to do?:    
+                1- Add card
+                2- Show cards
+                3- Contact people on the BaseContact list
+                4- Contact people on the BusinessContact list
+                5- Exit""")
+        x = input("Selected number: ")
     
    
-        if x == '1': organizerAnalityka.dodajWizytówkę()
-        if x == '2': organizerAnalityka.wyświetlWizytówki()
-        if x == '3': organizerAnalityka.kontaktbase()
-        if x == '4': organizerAnalityka.kontaktbusiness()
+        if x == '1': organizer.addCard()
+        if x == '2': organizer.showCards()
+        if x == '3': organizer.contactbase()
+        if x == '4': organizer.contactbusiness()
         if x == '5': 
             print("Exiting bye...")
             break
